f368fe93e339aada62205dc8719ee0abd534c7ac
BJ <213403019+BJ-BlackDuck@users.noreply.github.com>
2025-05-26 17:59:08 +0900

    1.0.0 initial commit

---
+++ app/routes/profile.js
@@ -0,0 +1,48 @@
+var ProfileDAO = require("../data/profile-dao").ProfileDAO;
+
+/* The ProfileHandler must be constructed with a connected db */
+function ProfileHandler(db) {
+    "use strict";
+
+    var profile = new ProfileDAO(db);
+
+    this.displayProfile = function(req, res, next) {
+
+        profile.getByUserId(req.session.userId, function(error, user) {
+
+            if (error) return next(error);
+
+            return res.render("profile", user);
+        });
+    };
+
+    this.handleProfileUpdate = function(req, res, next) {
+
+        var firstName = req.body.firstName;
+
+        //firstName = firstName.trim();
+
+        var lastName = req.body.lastName;
+        var ssn = req.body.ssn;
+        var dob = req.body.dob;
+        var address = req.body.address;
+        var userId = req.session.userId;
+
+        profile.updateUser(userId, firstName, lastName, ssn, dob, address, function(err, user) {
+
+            if (err) return next(err);
+
+            //firstName = firstName.trim();
+
+
+            user.updateSuccess = true;
+            user.userId = userId;
+
+            return res.render("profile", user);
+        });
+
+    };
+
+}
+
+module.exports = ProfileHandler;
